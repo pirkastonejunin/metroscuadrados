70d5c83da0967e6b8e9070cf9a0dd263711e9871
pirkastonejunin <junin@pirkastone.com>
2026-09-14 20:13:23 -0300

    Subir limite de body en express.json para permitir el payload de fotos del simulador de piso

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { MongoClient } = require('mongodb');
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '15mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const cotizadorRouter = require('./cotizador');
